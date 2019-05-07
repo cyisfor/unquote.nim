dacc1866493760a125e2b892afcab0522a164640
autocommit <autocommit>
2019-05-07 17:12:24 +0000

    auto 0 14 179

--- src/unquote.nim
+++ src/unquote.nim
@@ -8,39 +8,32 @@ type Accessor = seq[int]
 type Derp = tuple[name: string, acc: Accessor]
 
 proc accessors(exp: NimNode,
-               op: NimNodeKind = nnkPostfix,
+               op: string = "*",
                parent: Accessor = @[]): seq[Derp] =
-  var ident: NimNode
   debugEcho("kind ",exp.kind, " ", exp.repr)
-  if exp.kind == op:
-    case op:
-    of nnkAccQuoted:
-      ident = exp[0]
-      # other special cases here
-      # the typical thing is to have nnkSomething(uselessqualifier, ident) for any prefix ops
-      # and [x] is nnkBracketExpr(nothing, x) so 1 is the usual index of the ident
-    else:
-      ident = exp[1]
-    if ident.kind == nnkIdent:
-      result.add(($ident, parent))
-      return
+  if exp.kind == nnkPrefix and $exp[0] == op:
+    let ident = exp[1]
+    debugEcho("yay ",ident)
+    result.add(($ident, parent))
+    return
   for index in 0..<exp.len:
     var childacc = parent
     childacc.add(index)
+    debugEcho("childacc",childacc)
     for thing in accessors(exp[index], op, parent):
       result.add(thing)
 
-macro unquote(op: static[NimNodeKind] = nnkPostfix, exp: untyped): untyped =
+macro unquote(op: static[string] = "*", exp: untyped): untyped =
   debugEcho("wuh")
   for thing in accessors(exp, op):
     let name = thing[0]
     let accessor = thing[1]
-    debugEcho(name, accessor)
+    debugEcho("boing", name, accessor)
 
-unquote(nnkPostFix):
+unquote("*"):
   this
   iz
   a
-  $test
+  *test
   offf
   unquoting
