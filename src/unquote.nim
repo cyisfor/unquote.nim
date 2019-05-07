477bd15b2affeee73dea7d26f6f023a5af461e62
autocommit <autocommit>
2019-05-07 18:59:34 +0000

    auto 0 13 132

--- src/unquote.nim
+++ src/unquote.nim
@@ -17,10 +17,12 @@ from macros import NimNodeKind,
 type Accessor* = seq[int]
 type Derp = tuple[name: string, acc: Accessor]
 
-const debugging = false
+static:
+  const debugging {.booldefine.}: bool = false
 
-when debugging:
-  debugEcho("OSHTINETSH")
+  when debugging:
+    debugEcho("OSHTINETSH")
+
 
 proc default_checker*(ident: var NimNode, exp: NimNode): bool {.compileTime.} =
   if exp.kind != nnkAccQuoted:
@@ -36,9 +38,10 @@ proc unquote*(exp: NimNode,
              check: Checker = default_checker,
              parent: Accessor = @[]): seq[Derp] {.compileTime.} =
   var ident: NimNode
-  when debugging:
-    debugEcho("checking at",parent)
-    debugEcho(exp.repr)
+  static:
+    when debugging:
+      debugEcho("checking at",parent)
+      debugEcho(exp.repr)
   if check(ident, exp):
     result.add(($ident, parent))
     return
@@ -54,15 +57,17 @@ proc unquote*(exp: NimNode,
 {.hint[XDeclaredButNotUsed]: off.}    
 proc `[]`(exp: NimNode, acc: Accessor): NimNode {.compileTime.} =
   result = exp
-  when debugging:
-    debugEcho(acc)
-    debugEcho(result.treeRepr)
+  static:
+    when debugging:
+      debugEcho(acc)
+      debugEcho(result.treeRepr)
   for index in acc:
     result = result[index]
-    when debugging:
-      debugEcho("III",index)
-      debugEcho(result.repr)
-      debugEcho("=======")
+    static:
+      when debugging:
+        debugEcho("III",index)
+        debugEcho(result.repr)
+        debugEcho("=======")
 
 {.hint[XDeclaredButNotUsed]: off.}    
 proc `[]=`(exp: var NimNode, acc: Accessor, value: NimNode) {.compileTime.} =
@@ -72,16 +77,16 @@ proc `[]=`(exp: var NimNode, acc: Accessor, value: NimNode) {.compileTime.} =
   if len(acc) == 1:
     exp[acc[0]] = value
     return
-  when debugging:
-    debugEcho("set ACC ",acc)
-    debugEcho(exp.repr)
-    debugEcho("..........................")
+  static:
+    when debugging:
+      debugEcho("set ACC ",acc)
+      debugEcho(exp.repr)
+      debugEcho("..........................")
   var cur = exp
   for index in acc[0..^2]:
     cur = cur[index]
-    when debugging: debugEcho("at ", index, ' ', cur.repr)
-  when debugging:
-    debugEcho("setting at ", acc[acc.len-1], " ", cur[acc[acc.len-1]].repr)
+    static: when debugging: debugEcho("at ", index, ' ', cur.repr)
+  static: when debugging: debugEcho("setting at ", acc[acc.len-1], " ", cur[acc[acc.len-1]].repr)
   cur[acc[acc.len-1]] = value
   when debugging:
     debugEcho(exp.repr)
