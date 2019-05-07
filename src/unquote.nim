dfa1c4185622f659500eb2da8f1fcef4d4c06114
autocommit <autocommit>
2019-05-07 17:08:07 +0000

    auto 0 8 203

--- src/unquote.nim
+++ src/unquote.nim
@@ -9,30 +9,30 @@ type Derp = tuple[name: string, acc: Accessor]
 
 proc accessors(exp: NimNode,
                op: NimNodeKind = nnkPostfix,
-               parent: Accessor = @[]): iterator(): Derp =
-  iterator it(): Derp {.closure.} =
-    var ident: NimNode
-    if exp.kind == op:
-      case op:
-      of nnkAccQuoted:
-        ident = exp[0]
-        # other special cases here
-        # the typical thing is to have nnkSomething(uselessqualifier, ident) for any prefix ops
-        # and [x] is nnkBracketExpr(nothing, x) so 1 is the usual index of the ident
-      else:
-        ident = exp[1]
-      if ident.kind == nnkIdent:
-        yield ($ident, parent)
-    for index in 0..<exp.len:
-      var childacc = parent
-      childacc.add(index)
-      for thing in accessors(exp[index], op, parent)():
-        yield thing
-  return it
+               parent: Accessor = @[]): seq[Derp] =
+  var ident: NimNode
+  debugEcho("kind ",exp.kind, " ", exp.repr)
+  if exp.kind == op:
+    case op:
+    of nnkAccQuoted:
+      ident = exp[0]
+      # other special cases here
+      # the typical thing is to have nnkSomething(uselessqualifier, ident) for any prefix ops
+      # and [x] is nnkBracketExpr(nothing, x) so 1 is the usual index of the ident
+    else:
+      ident = exp[1]
+    if ident.kind == nnkIdent:
+      result.add(($ident, parent))
+      return
+  for index in 0..<exp.len:
+    var childacc = parent
+    childacc.add(index)
+    for thing in accessors(exp[index], op, parent):
+      result.add(thing)
 
 macro unquote(op: static[NimNodeKind] = nnkPostfix, exp: untyped): untyped =
   debugEcho("wuh")
-  for thing in accessors(exp, op)():
+  for thing in accessors(exp, op):
     let name = thing[0]
     let accessor = thing[1]
     debugEcho(name, accessor)
@@ -41,6 +41,6 @@ unquote(nnkPostFix):
   this
   iz
   a
-  test*
+  $test
   offf
   unquoting
