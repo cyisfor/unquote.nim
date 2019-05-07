26d4dead5dd32784f21345f2fcb51d551fd6e8b2
autocommit <autocommit>
2019-05-07 17:32:12 +0000

    auto 0 17 200

--- src/unquote.nim
+++ src/unquote.nim
@@ -29,7 +29,7 @@ proc accessors(exp: NimNode,
     return
   for index in 0..<exp.len:
     var childacc = parent
-    childacc.add(index)
+    childacc.insert(0,index)
     debugEcho("childacc",childacc)
     result.add(accessors(exp[index], op, kind, childacc))
 
@@ -39,6 +39,15 @@ type Opts = tuple
 
 proc opts(op: string = "*", kind: NimNodeKind = nnkPrefix): Opts {.compileTime.} =
   return (op: op, kind: kind)
+
+proc `[]`(exp: NimNode, acc: Accessor): NimNode {.compileTime.} =
+  if len(acc) == 1:
+    return exp[acc[0]]
+  return exp[acc[1..^0]]
+proc `[]=`(exp: NimNode, acc: Accessor, value: NimNode): NimNode {.compileTime.} =
+  if len(acc) == 1:
+    exp[acc[0]] = value
+  exp[acc[1..^0]] = value
   
 macro unquote(opts: static[Opts], exp: untyped): untyped =
   result = newNimNode(nnkStmtList)
@@ -46,7 +55,7 @@ macro unquote(opts: static[Opts], exp: untyped): untyped =
   for thing in accessors(exp, opts.op, opts.kind):
     let name = newIdentNode(thing[0])
     let accessor = thing[1]
-    debugEcho("boing", name, accessor)
+    debugEcho("boing", name, accessor, exp[accessor].repr)
     let expr = quote:
       let `name`: Accessor = @`accessor`
     debugEcho(expr.repr)
